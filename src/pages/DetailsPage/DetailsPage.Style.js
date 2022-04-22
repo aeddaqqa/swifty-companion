c230f1ed0b52cbe9b043d70564d23835947a7b9f
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-04-22 02:23:15 +0000

    add projects and skills

--- src/pages/DetailsPage/DetailsPage.Style.js
+++ src/pages/DetailsPage/DetailsPage.Style.js
@@ -1,10 +1,12 @@
 import styled from "styled-components/native";
 
-export const StyledView = styled.ScrollView``;
+export const StyledView = styled.ScrollView`
+    /* padding: 20px; */
+`;
 
 export const StyledUserInfo = styled.View`
     width: 100%;
-    height: 300px;
+    height: 200px;
     display: flex;
     flex-direction: row;
     padding: 10px;
@@ -36,21 +38,25 @@ export const StyledInfoBox = styled.View`
 
 export const StyledText = styled.Text`
     font-size: 16px;
+    /* padding: 5px; */
 `;
 
 export const StyledCoalitionContainer = styled.View`
     border: 3px solid black;
-    width: 90%;
-    height: 200px;
+    /* width: 90%; */
+    flex-basis: 45%;
+    height: 100px;
     margin: 0 auto;
-    margin-bottom: 20px;
     display: flex;
     align-items: center;
     justify-content: space-around;
+    padding-bottom: 10px;
 `;
 
 export const StyledPikala = styled.View`
     width: 100%;
+    /* flex: 1; */
     height: 300px;
-    margin-bottom: 20px;
+    /* margin-bottom: 20px; */
+    padding: 0 20px;
 `;
--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -15,9 +15,22 @@ import SwitchSelector from "react-native-switch-selector";
 import SelectDropdown from "react-native-select-dropdown";
 import { SvgUri } from "react-native-svg";
 
-const Item = ({ title }) => (
+const Skills = ({ item }) => (
     <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
+        <StyledText>{item.name}</StyledText>
+        <StyledText>{item.level}</StyledText>
+        {/* <Text style={styles.title}>{title}</Text> */}
+    </View>
+);
+const Projects = ({ item }) => (
+    <View style={styles.item}>
+        <StyledText>{item.project.name}</StyledText>
+        {/* <StyledText>{item.status}</StyledText> */}
+        {item.status == "finished" ? (
+            <StyledText>{item.final_mark}</StyledText>
+        ) : (
+            <StyledText>{item.status}</StyledText>
+        )}
     </View>
 );
 
@@ -25,12 +38,12 @@ const Details = (props) => {
     const { state } = useLocation();
     const [cursus, setCursus] = useState(null);
     const [selectedData, setSelectedData] = useState("p");
-    const countries = ["Egypt", "Canada", "Australia", "Ireland"];
     // console.log(state.cursus);
     useEffect(() => {
-        console.log(cursus?.skills);
+        console.log(cursus?.projects);
     }, [cursus]);
-    const renderItem = ({ item }) => <Item title={item.title} />;
+    const renderSkills = ({ item }) => <Skills item={item} />;
+    const renderProjects = ({ item }) => <Projects item={item} />;
     return (
         <StyledView>
             <StyledUserInfo>
@@ -59,56 +72,64 @@ const Details = (props) => {
                     ) : null}
                 </StyledInfoBox>
             </StyledUserInfo>
-            <View style={styles.dropDown}>
-                <StyledText>Cursus : </StyledText>
-                <SelectDropdown
-                    defaultButtonText="Select Cursus"
-                    data={state.cursus}
-                    onSelect={(selectedItem, index) => {
-                        setCursus(selectedItem);
-                        console.log(selectedItem.name, index);
-                    }}
-                    buttonTextAfterSelection={(selectedItem, index) => {
-                        // text represented after item is selected
-                        // if data array is an array of objects then return selectedItem.property to render after item is selected
-                        return selectedItem.name;
-                    }}
-                    rowTextForSelection={(item, index) => {
-                        // text represented for each item in dropdown
-                        // if data array is an array of objects then return item.property to represent item in dropdown
-                        return item.name;
-                    }}
-                />
-            </View>
-            <StyledCoalitionContainer
-                style={{ backgroundColor: cursus?.coalition?.color }}
-            >
-                {cursus?.coalition?.hascoalition ? (
-                    <SvgUri
-                        width="50%"
-                        height="50%"
-                        uri={cursus.coalition.image_url}
-                        fill={"#ffffff"}
+            <View style={styles.cursusContainer}>
+                <View style={styles.dropDown}>
+                    <StyledText>Cursus : </StyledText>
+                    <SelectDropdown
+                        defaultButtonText="Select Cursus"
+                        buttonStyle={{
+                            height: 40,
+                            width: "60%",
+                        }}
+                        data={state.cursus}
+                        onSelect={(selectedItem, index) => {
+                            setCursus(selectedItem);
+                            console.log(selectedItem.name, index);
+                        }}
+                        buttonTextAfterSelection={(selectedItem, index) => {
+                            // text represented after item is selected
+                            // if data array is an array of objects then return selectedItem.property to render after item is selected
+                            return selectedItem.name;
+                        }}
+                        rowTextForSelection={(item, index) => {
+                            // text represented for each item in dropdown
+                            // if data array is an array of objects then return item.property to represent item in dropdown
+                            return item.name;
+                        }}
                     />
-                ) : (
-                    <>
+                </View>
+                <StyledCoalitionContainer
+                    style={{ backgroundColor: cursus?.coalition?.color }}
+                >
+                    {cursus?.coalition?.hascoalition ? (
                         <SvgUri
-                            width="60%"
-                            height="60%"
-                            uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+                            width="50%"
+                            height="50%"
+                            uri={cursus.coalition.image_url}
+                            fill={"#ffffff"}
                         />
-                        <StyledText>No Coalition</StyledText>
-                    </>
-                )}
-                {cursus?.coalition?.hascoalition && (
-                    <>
-                        <StyledText>Name : {cursus.coalition.name}</StyledText>
-                        <StyledText>
-                            Score : {cursus.coalition.score}
-                        </StyledText>
-                    </>
-                )}
-            </StyledCoalitionContainer>
+                    ) : (
+                        <>
+                            <SvgUri
+                                width="60%"
+                                height="60%"
+                                uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+                            />
+                            <StyledText>No Coalition</StyledText>
+                        </>
+                    )}
+                    {cursus?.coalition?.hascoalition && (
+                        <>
+                            <StyledText>
+                                Name : {cursus.coalition.name}
+                            </StyledText>
+                            <StyledText>
+                                Score : {cursus.coalition.score}
+                            </StyledText>
+                        </>
+                    )}
+                </StyledCoalitionContainer>
+            </View>
             <SwitchSelector
                 initial={0}
                 onPress={(value) => setSelectedData(value)}
@@ -116,26 +137,45 @@ const Details = (props) => {
                 options={[
                     {
                         label: "projects",
-                        value: "p",
+                        value: 0,
                     },
                     {
                         label: "Skills",
-                        value: "s",
+                        value: 1,
                     },
                 ]}
                 testID="gender-switch-selector"
                 accessibilityLabel="gender-switch-selector"
-                style={{ width: "90%", height: "10%", margin: "0% auto" }}
+                style={{
+                    width: "100%",
+                    height: "10%",
+                    paddingLeft: "10%",
+                    paddingRight: "10%",
+                }}
             />
-            <StyledPikala>
-                {cursus?.skills?.length > 0 ? (
-                    <FlatList
-                        data={cursus.projects}
-                        renderItem={renderItem}
-                        keyExtractor={(item) => item.id}
-                    />
-                ) : null}
-            </StyledPikala>
+            {selectedData == 1 ? (
+                <StyledPikala>
+                    {cursus?.skills?.length > 0 ? (
+                        <FlatList
+                            style={{ flex: 1 }}
+                            data={cursus.skills}
+                            renderItem={renderSkills}
+                            keyExtractor={(item) => item.id}
+                        />
+                    ) : null}
+                </StyledPikala>
+            ) : (
+                <StyledPikala>
+                    {cursus?.projects?.length > 0 ? (
+                        <FlatList
+                            style={{ flex: 1 }}
+                            data={cursus.projects}
+                            renderItem={renderProjects}
+                            keyExtractor={(item) => item.id}
+                        />
+                    ) : null}
+                </StyledPikala>
+            )}
         </StyledView>
     );
 };
@@ -144,20 +184,44 @@ const styles = StyleSheet.create({
     dropDown: {
         display: "flex",
         flexDirection: "row",
-        marginTop: "-10%",
-        width: "100%",
+        paddingLeft: 20,
         height: "15%",
         alignItems: "center",
         justifyContent: "center",
+        flexBasis: "45%",
+        flexWrap: "wrap",
     },
     item: {
-        backgroundColor: "#f9c2ff",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "#FFFAFA",
         padding: 20,
+        flexWrap: "wrap",
         marginVertical: 8,
         marginHorizontal: 16,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
     },
     title: {
-        fontSize: 32,
+        // fontSize: 32,
+    },
+    cursusContainer: {
+        marginBottom: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        // flexWrap: "wrap",
+        // margin: "0% auto",
     },
 });
 
